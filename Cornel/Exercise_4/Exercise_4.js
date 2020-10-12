d3880fe0398a9302dc95e0815392e21a06ce9627
Cornel Gurin <cornelgurin@gmail.com>
2020-10-12 13:01:54 +0200

    commit

---
+++ Cornel/Exercise_4/Exercise_4.js
@@ -0,0 +1,12 @@
+var name_1 = "Hi my name is";
+var name_2 = " Cornel";
+var name_3 = " and i am 25 years old";
+var text = name_1 + name_2 + name_3;
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById( "paragraph" ).innerHTML = text ;
+});
+
+ 
