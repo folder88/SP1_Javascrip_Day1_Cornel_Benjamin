2eaa9bb58f0d77ac139b51c5bbfdb8e8dcb7bb93
Cornel Gurin <cornelgurin@gmail.com>
2020-10-12 21:04:37 +0200

    commit

--- Cornel/Exercise_5/Exercises_5.js
+++ Cornel/Exercise_5/Exercises_5.js
@@ -1,4 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById( "p1" ).innerHTML = "text" ;
     
+var x =  document.getElementsByClassName("p");
+
+    x[0].innerHTML="Selector by Classname";
+    x[1].innerHTML="Selector by Classname";
+    x[0].style="background-color:blue;";
+var y = document.getElementsByTagName("h3");
+y[0].innerHTML="Select by Tagname";
+
+
+
 });
