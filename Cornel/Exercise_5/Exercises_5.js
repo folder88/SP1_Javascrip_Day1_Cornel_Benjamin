0b80175df8cc6d01af103a1e15a141c719a72918
Cornel Gurin <cornelgurin@gmail.com>
2020-10-12 13:51:59 +0200

    commit

---
+++ Cornel/Exercise_5/Exercises_5.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById( "p1" ).innerHTML = "text" ;
+    
+});
---
+++ Cornel/Exercises_6/Exercises_6.js
@@ -0,0 +1,8 @@
+var array = [[1,2,1,24],[8,11,9,4],[7,0,7,27],[7,4,28,14],[3,10,25,7],[21,4,6,17],[3,5,26,3]];
+console.log(array[1][1],array[4][2],array[5][3],array[2][3],array[2][1]);
+var array2 = [array[1][1],array[4][2],array[5][3],array[2][3],array[2][1]];
+console.log(array2);
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById( "p" ).innerHTML = array2 ;
+    
+});
